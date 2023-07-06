258f1d53f3505a144f68b81c15ca7b91c03c853b
Spikoftw <cyril.cantonnet@gmail.com>
2023-07-06 19:11:56 +0200

    Comportement fleches + ajout bulletpoints

--- assets/script.js
+++ assets/script.js
@@ -20,21 +20,42 @@ const slides = [
 
 let flechesGauche = document.querySelector(".arrow_left")
 let flecheDroite = document.querySelector(".arrow_right")
-flechesGauche.addEventListener("click", () => {
-	console.log("J'ai cliqué sur la fleche de gauche")
-})
-flecheDroite.addEventListener("click", () => {
-	console.log("J'ai cliqué sur la fleche de droite")
-})
-
-
-
-
+let bannerImg = document.querySelector(".banner-img")
+let index = 0
 let dots = document.querySelector(".dots")
+let textBanner = document.querySelector("#banner p")
+
+bannerImg.src = "./assets/images/slideshow/" + slides[0].image
 
 for (i = 0; i < slides.length; i++) {
 	let nouvelElement = document.createElement("div")
 	nouvelElement.classList.add("dot")
 	dots.appendChild(nouvelElement)
-
 }
+
+let dotselected = document.querySelector(".dot")
+
+flechesGauche.addEventListener("click", () => {
+	console.log("J'ai cliqué sur la fleche de gauche")
+	if (index === 0) {
+		index = slides.length - 1
+	}
+	else {
+		index--
+	}
+	dotselected.classList.add("dot_selected")
+	textBanner.innerHTML = slides[index].tagLine
+	bannerImg.src = "./assets/images/slideshow/" + slides[index].image
+})
+
+flecheDroite.addEventListener("click", () => {
+	console.log("J'ai cliqué sur la fleche de droite")
+	if (index < slides.length - 1) {
+		index++
+	}
+	else {
+		index = 0
+	}
+	textBanner.innerHTML = slides[index].tagLine
+	bannerImg.src = "./assets/images/slideshow/" + slides[index].image
+})
